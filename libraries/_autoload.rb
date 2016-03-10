29cd9dd87de443fb321a58b3eee31803a0c5c431
Mikko Caldara <mikko.caldara@cloudreach.co.uk>
2016-03-10 17:26:42 +0000

    Add custom gem sources capability
    In locked down environments, Rubygems might not be
    reachable. A private gem mirror can be used instead.
    As per https://docs.chef.io/resource_chef_gem.html#properties

--- libraries/_autoload.rb
+++ libraries/_autoload.rb
@@ -7,5 +7,9 @@ rescue LoadError
 
   docker = Chef::Resource::ChefGem.new('docker-api', run_context)
   docker.version '= 1.26.2'
+  unless node['docker']['gem_source'].nil?
+    docker.source node['docker']['gem_source']
+    docker.clear_sources true
+  end
   docker.run_action(:install)
 end
